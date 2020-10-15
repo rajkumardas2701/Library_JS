de98674e34388450c92096b6e035e105080d8746
rajkumardas2701 <41339878+rajkumardas2701@users.noreply.github.com>
2020-10-15 05:41:35 +0530

    adding render function to display books

---
+++ app.js
@@ -0,0 +1,29 @@
+const myLibrary = [];
+
+// function Book() {
+
+// }
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
+
+function render() {
+  let result = '';
+  for (let i = 0; i < myLibrary.length; i++) {
+    result += `<div class="container">
+    <div class="mt-4 mb-5 p-5 h-25 border w-25 book-div" id="book-modal">
+      <label for="title">
+        ${myLibrary[i]}
+    </label>
+    </div>
+  </div>`;
+  }
+  document.getElementById('book-list').innerHTML = result;
+}
+
+addBookToLibrary('test book');
+addBookToLibrary('test1 book');
+addBookToLibrary('test2 book');
+
+render();
