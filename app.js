e57a8e65452dd4fcad23b45fa324411006917219
rajkumardas2701 <41339878+rajkumardas2701@users.noreply.github.com>
2020-10-21 05:04:06 +0530

    Adding function to add book to myLibrary Array

--- app.js
+++ app.js
@@ -2,18 +2,18 @@ const myLibrary = [];
 
 const bookForm = document.createElement('form');
 bookForm.innerHTML = `
-<label htmlFor="title">title</label>
-<input type="text" name="title" id="title"/>
-<label htmlFor="Author">Author</label>
-<input type="text" name="Author" id="author"/>
-<label htmlFor="Rating">Rating</label>
-<input type="number" min="1" max="5" name="rating" id="rating"/>
+<label htmlFor="title">Title: </label>
+<input type="text" name="title" id="title"/><br>
+<label htmlFor="Author">Author: </label>
+<input type="text" name="Author" id="author"/><br>
+<label htmlFor="Rating">Rating: </label>
+<input type="number" min="1" max="5" name="rating" id="rating"/><br>
 <div class="is-read">
   <label htmlFor="isRead">Have you read it?</label>
   <input type="checkbox" name="isRead" id="isRead"/>
-  </div>
+  </div><br>
 <label htmlFor="pages">How many pages?</label>
-<input type="number" min="1" max="10000" name="pages" id="pages"/>
+<input type="number" min="1" max="10000" name="pages" id="pages"/><br>
 <button type="submit">Add</button>
 `;
 
@@ -21,12 +21,19 @@ function renderForm() {
   document.body.appendChild(bookForm);
 }
 
-const formButton = document.getElementById('new-book').addEventListener('click', renderForm);
-document.body.insertAdjacentElement('beforeend', formButton);
+const formButton = document.getElementById('new-book');
+formButton.addEventListener('click', renderForm);
+// document.body.insertAdjacentElement('beforeend', formButton);
 
-// function Book() {
-
-// }
+class Book {
+  constructor(title, author, rating, isRead, pages) {
+    this.title = title;
+    this.author = author;
+    this.rating = rating;
+    this.isRead = isRead;
+    this.pages = pages;
+  }
+}
 
 function render() {
   let result = '';
@@ -34,8 +41,20 @@ function render() {
     result += `<div class="container">
     <div class="mt-4 mb-5 p-5 h-25 border w-25 book-div" id="book-modal">
       <label for="title">
-        ${myLibrary[i]}
-    </label>
+        Title: ${myLibrary[i].title}
+      </label>
+      <label for="title">
+        Author: ${myLibrary[i].author}
+      </label>
+      <label for="title">
+        Rating: ${myLibrary[i].rating}
+      </label>
+      <label for="title">
+        Read: ${myLibrary[i].isRead}
+      </label>
+      <label for="title">
+        Pages: ${myLibrary[i].pages}
+      </label>
     </div>
   </div>`;
   }
@@ -43,13 +62,29 @@ function render() {
 }
 
 function addBookToLibrary(book) {
-
-
   myLibrary.push(book);
+  // console.log(myLibrary);
   render();
 }
 
+function addNewBook(e) {
+  // alert('Inside Add New Book function');
+  e.preventDefault();
+  const book = new Book(
+    e.target.title.value,
+    e.target.author.value,
+    e.target.rating.value,
+    e.target.isRead.value,
+    e.target.pages.value,
+  );
+  // console.log(book);
+  addBookToLibrary(book);
+}
+
+// bookForm.addEventListener('submit', addNewBook);
+
+bookForm.addEventListener('submit', addNewBook);
 // addBookToLibrary('test book');
 // addBookToLibrary('test1 book');
 // addBookToLibrary('test2 book');
-
+// render();
